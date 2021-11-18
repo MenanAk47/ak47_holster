def8971ac1a739297397b333f99c3423cda83de7
MenanAk47 <35970476+MenanAk47@users.noreply.github.com>
2021-11-18 11:47:41 +0600

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,5 +1,9 @@
 fx_version 'bodacious'
 game 'gta5'
+name "ak47_holster"
+author "MenanAk47 (MenanAk47#3129)"
+description "MenanAk47 - Holster (Beta)"
+version "1.0"
 
 client_scripts {
 	'config.lua',
